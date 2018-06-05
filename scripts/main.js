f91b33d80599141e36fe6bc6d57e70351517340a
Nate <ndsunc@gmail.com>
2018-06-04 20:35:50 -0700

    Added local stoage and, if it exists, the page laods with the previous game's informaiton.

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,30 @@ const r4Input = document.querySelector('.color4-r')
 const g4Input = document.querySelector('.color4-g')
 const b4Input = document.querySelector('.color4-b')
 
+var storedCornerInfo = localStorage.getItem('4corners')
+if (storedCornerInfo) {
+  let previousGame = JSON.parse(storedCornerInfo)
+
+  r1Input.value = previousGame['bl'][0]
+  g1Input.value = previousGame['bl'][1]
+  b1Input.value = previousGame['bl'][2]
+  r2Input.value = previousGame['br'][0]
+  g2Input.value = previousGame['br'][1]
+  b2Input.value = previousGame['br'][2]
+  r3Input.value = previousGame['tl'][0]
+  g3Input.value = previousGame['tl'][1]
+  b3Input.value = previousGame['tl'][2]
+  r4Input.value = previousGame['tr'][0]
+  g4Input.value = previousGame['tr'][1]
+  b4Input.value = previousGame['tr'][2]
+
+  updateColorSample1()
+  updateColorSample2()
+  updateColorSample3()
+  updateColorSample4()
+}
+
+
 // Update color swatch when change RGB values
 function updateColorSample1 () {
   const rVal = r1Input.value
@@ -68,8 +92,25 @@ b4Input.addEventListener('change', updateColorSample4)
 submit.addEventListener('click', (event) => {
   event.preventDefault()
 
-  // localstorage save color info
-  // do grid stuff
-  // swap hidden div attributes (I think that's how I'll do it)
-  // display grid
+  const r1Val = parseInt(r1Input.value)
+  const g1Val = parseInt(g1Input.value)
+  const b1Val = parseInt(b1Input.value)
+  const r2Val = parseInt(r2Input.value)
+  const g2Val = parseInt(g2Input.value)
+  const b2Val = parseInt(b2Input.value)
+  const r3Val = parseInt(r3Input.value)
+  const g3Val = parseInt(g3Input.value)
+  const b3Val = parseInt(b3Input.value)
+  const r4Val = parseInt(r4Input.value)
+  const g4Val = parseInt(g4Input.value)
+  const b4Val = parseInt(b4Input.value)
+
+  const cornersInfo = {
+    'tl': [r1Val, g1Val, b1Val],
+    'tr': [r2Val, g2Val, b2Val],
+    'bl': [r3Val, g3Val, b3Val],
+    'br': [r4Val, g4Val, b4Val]
+  }
+
+  localStorage.setItem('4corners', JSON.stringify(cornersInfo))
 })
