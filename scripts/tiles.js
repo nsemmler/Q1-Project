162fdbea0c18971d38051c0996ae418f56d26d41
Nate <ndsunc@gmail.com>
2018-06-06 14:46:49 -0700

    Created #createSolutionGrid to mimic actions of #createGrid with a new template

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -9,13 +9,13 @@ function createGrid () {
   })
 }
 
-// function createSolutionGrid () {
-//   let array = Array.from(Array(60).keys())
-//   let acc = 1
-//
-//   array.forEach((tile) => {
-//     const grid = document.querySelector('.grid-solution')
-//     grid.innerHTML += gridTemplate(acc)
-//     acc++
-//   })
-// }
+function createSolutionGrid (originalGrid) {
+  let array = Array.from(Array(60).keys())
+  let acc = 1
+
+  array.forEach((tile) => {
+    const solution = document.querySelector('.solution')
+    solution.innerHTML += solutionTemplate(acc)
+    acc++
+  })
+}
