1a71c009de3122e7218702923986eaf2430f5cb2
Nate <ndsunc@gmail.com>
2018-06-06 14:47:05 -0700

    Added new template with different class and ID names for solution grid

--- scripts/templates.js
+++ scripts/templates.js
@@ -15,3 +15,21 @@ function gridTemplate (acc) {
 
   return answer
 }
+
+function solutionTemplate (acc) {
+  let answer = ''
+
+  if (acc === 1) {
+    answer = `<div class="soln-tile tl fixedtile" id="S${acc}"><i class="fas fa-circle"></i></div>`
+  } else if (acc === 6) {
+    answer = `<div class="soln-tile tr fixedtile" id="S${acc}"><i class="fas fa-circle"></i></div>`
+  } else if (acc === 55) {
+    answer = `<div class="soln-tile bl fixedtile" id="S${acc}"><i class="fas fa-circle"></i></div>`
+  } else if (acc === 60) {
+    answer = `<div class="soln-tile br fixedtile" id="S${acc}"><i class="fas fa-circle"></i></div>`
+  } else {
+    answer = `<div class="soln-tile" id="S${acc}"></div>`
+  }
+
+  return answer
+}
