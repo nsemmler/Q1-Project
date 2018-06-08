c3fd1faae94b8cdc0656d5e4c66eda291dc2522a
Nate <ndsunc@gmail.com>
2018-06-07 22:02:03 -0700

    Moved tile functionality into tiles.js, refactored, added comments to most actions

--- scripts/main.js
+++ scripts/main.js
@@ -1,263 +1,13 @@
+const instructionsdiv = document.querySelector('.rules-header-and-icon')
 const rules = document.querySelector('.rules')
-const rulesbtn = document.querySelector('.instructionsbtn')
-const rulesicon = document.querySelector('.rulesicon')
-const submit = document.querySelector('.create-grid')
-const mirror = document.querySelector('.mirror')
-const hint = document.querySelector('.hint')
 
+// if localStorage exists, fill in User colors with previous
 var storedCornerInfo = localStorage.getItem('4corners')
 var originalGrid = {}
 let corners = ''
 if (storedCornerInfo) {
   corners = JSON.parse(storedCornerInfo)
   fillInColorsWithPreviousColors(corners)
-}
-
-// Click Submit button:
-submit.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  const color1 = document.querySelector('.color1')
-  const color2 = document.querySelector('.color2')
-  const color3 = document.querySelector('.color3')
-  const color4 = document.querySelector('.color4')
-
-  tl = hexToRgb(color1.value)
-  tr = hexToRgb(color2.value)
-  bl = hexToRgb(color3.value)
-  br = hexToRgb(color4.value)
-
-  const cornersInfo = {
-    'tl': tl,
-    'tr': tr,
-    'bl': bl,
-    'br': br
-  }
-
-  localStorage.setItem('4corners', JSON.stringify(cornersInfo))
-  originalGrid = generateGrid(6, 10)
-
-  createSolutionGrid(originalGrid)
-  colorInSolutionGrid()
-  toggleFixedTileIconColor()
-  animateGridCountdownToShuffle()
-
-  setTimeout(() => {
-    scrambleGrid()
-    addClickEventsToTiles()
-    mirror.style.display = 'block'
-    hint.style.display = 'block'
-  }, 3000)
-})
-
-// Click Show Instructions - button or icon next to it:
-rulesbtn.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  if (rules.classList.contains('show')) {
-    rulesbtn.textContent = 'View Instructions'
-    rulesicon.className = 'fas fa-angle-down rulesicon'
-  } else {
-    rulesbtn.textContent = 'Hide Instructions'
-    rulesicon.className = 'fas fa-angle-up rulesicon'
-  }
-})
-
-rulesicon.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  if (rules.classList.contains('show')) {
-    rulesbtn.textContent = 'View Instructions'
-    rulesicon.className = 'fas fa-angle-down rulesicon'
-  } else {
-    rulesbtn.textContent = 'Hide Instructions'
-    rulesicon.className = 'fas fa-angle-up rulesicon'
-  }
-})
-
-// Click Mirror button:
-mirror.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  const solution = document.querySelector('.solution')
-  if (solution.style.display === 'none') {
-    solution.style.display = 'grid'
-    mirror.textContent = 'Return to Game'
-  } else {
-    solution.style.display = 'none'
-    mirror.textContent = 'Show Solution'
-  }
-})
-
-// Click Hint button:
-hint.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  const progress = document.querySelector('.progress')
-  if (progress.style.opacity === '0') {
-    progress.style.opacity = '1'
-
-    // const alert = document.querySelector('.alert')
-    // if (alert.style.display === 'none') progress.style.marginTop = '15px'
-  } else {
-    progress.style.opacity = '0'
-  }
-})
-
-// Converters between HEX & RGB
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  const rVal = parseInt(result[1], 16)
-  const gVal = parseInt(result[2], 16)
-  const bVal = parseInt(result[3], 16)
-
-  return result ? [rVal, gVal, bVal] : null
-}
-
-function componentToHex(c) {
-  const hex = c.toString(16)
-  return hex.length == 1 ? "0" + hex : hex
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
-}
-
-// Generates empty <divs> within the grid
-createGrid()
-
-// Swap colors of two tiles
-let firstColor
-let secondColor
-let correctTilesObj = {}
-
-function addClickEventsToTiles () {
-  const allTiles = document.querySelectorAll('.tile')
-  const tiles = Array.from(allTiles)
-
-  tiles.forEach((tile) => {
-    if (tile.classList.contains('fixedtile')) {
-      removeCornerTilesFromGettingClickEvents(tiles, tile)
-    }
-  })
-
-  tiles.forEach((tile) => {
-    tile.addEventListener('click', (event) => {
-      event.preventDefault()
-
-      // Toggle 'selected' of same tile when clicked
-      if (tile.classList.contains('selected')) {
-        tile.classList.remove('selected')
-      } else {
-        tile.classList.add('selected')
-      }
-
-      const selectedTilesInfo = document.querySelectorAll('.selected')
-      let selectedTiles = Array.from(selectedTilesInfo)
-
-      if (selectedTiles.length === 2) {
-        const firstTile = selectedTiles[0]
-        const secondTile = selectedTiles[1]
-
-        firstColor = firstTile.style.background
-        secondColor = secondTile.style.background
-        firstTile.style.background = secondColor
-        secondTile.style.background = firstColor
-
-        // animateTileSwap(firstTile, secondTile)
-
-        firstTile.classList.remove('selected')
-        secondTile.classList.remove('selected')
-
-        checkUserAnswer(correctTilesObj)
-      }
-    })
-  })
-}
-
-// Shuffle grid
-let swapColorsObj = {}
-let swappableIndexes = []
-
-function scrambleGrid () {
-  const allTiles = document.querySelectorAll('.tile')
-  const tiles = Array.from(allTiles)
-
-  tiles.forEach((tile) => {
-    if (tile.classList.length === 1) { // don't swap the corners
-      swapColorsObj[tile.id] = {'oldColor': tile.style.background, 'newColor': null }
-    }
-  })
-
-  for (let index in swapColorsObj) { swappableIndexes.push(index) }
-
-  swappableIndexes = shuffle(swappableIndexes)
-  let swappedIndex = 0
-
-  for (let index in swapColorsObj) {
-    const otherTileID = parseInt(swappableIndexes[swappedIndex])
-    const otherTileColor = swapColorsObj[otherTileID]['oldColor']
-    swapColorsObj[index]['newColor'] = otherTileColor
-    swappedIndex++
-  }
-
-  tiles.forEach((tile) => {
-    if (tile.classList.length === 1) {
-      tile.style.background = swapColorsObj[tile.id]['newColor']
-    }
-  })
-}
-
-// Fisher-Yates shuffle algorithm
-function shuffle (array) {
-  var j, x, i;
-  for (i = array.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = array[i];
-      array[i] = array[j];
-      array[j] = x;
-  }
-
-  return array;
-}
-
-function checkUserAnswer (correctTilesObj) {
-  const allTiles = document.querySelectorAll('.tile')
-  const tiles = Array.from(allTiles)
-  let originalIndex = 1
-
-  tiles.forEach((tile) => {
-    const originalTileColor = originalGrid[originalIndex]
-    const currentTileColor = tile.style.background
-
-    if (currentTileColor == originalTileColor) {
-      correctTilesObj[originalIndex] = true
-    } else {
-      correctTilesObj[originalIndex] = false
-    }
-
-    originalIndex++
-  })
-
-  updateHintValue(correctTilesObj)
-
-  let correctSolution = [...new Set(Object.values(correctTilesObj))]
-  if (correctSolution.length === 1 && correctSolution[0] === true) {
-    const alert = document.querySelector('.winner-alert')
-
-    if (alert.style.display == 'none') {
-      alert.style.display = 'flex'
-
-      setTimeout(() => {
-        alert.style.display = 'none'
-      }, 5000)
-    }
-  }
-}
-
-function removeCornerTilesFromGettingClickEvents (array, element) {
-  const index = array.indexOf(element)
-  array.splice(index, 1)
 }
 
 function fillInColorsWithPreviousColors (corners) {
@@ -277,50 +27,21 @@ function fillInColorsWithPreviousColors (corners) {
   color4.value = prevColor4
 }
 
-function toggleFixedTileIconColor () {
-  const fixedIcons = document.querySelectorAll('.fa-circle')
-  const icons = Array.from(fixedIcons)
+// Generates empty <divs> within the grid
+createGrid()
 
-  icons.forEach((icon) => {
-    icon.style.color = 'black'
-  })
+// Checks if user won after every tile swap
+function checkUserAnswer (correctTilesObj) {
+  const allTiles = document.querySelectorAll('.tile')
+  const tiles = Array.from(allTiles)
+  let originalIndex = 1
+
+  compareTileColors(tiles, originalIndex)
+  updateHintValue(correctTilesObj)
+  displayBannerIfWon(correctTilesObj)
 }
 
-function animateGridCountdownToShuffle () {
-  const grid = document.querySelector('.grid')
-
-  grid.style.transform = 'scale(1.05)'
-  setTimeout(() => {
-    grid.style.transform = 'scale(1)'
-
-    setTimeout(() => {
-      grid.style.transform = 'scale(1.05)'
-
-      setTimeout(() => {
-        grid.style.transform = 'scale(1)'
-
-        setTimeout(() => {
-          grid.style.transform = 'scale(1.05)'
-
-          setTimeout(() => {
-            grid.style.transform = 'scale(1)'
-          }, 500)
-        }, 500)
-      }, 500)
-    }, 500)
-  }, 500)
-}
-
-function colorInSolutionGrid () {
-  const solutionTiles = document.querySelectorAll('.soln-tile')
-  const solution = Array.from(solutionTiles)
-
-  solution.forEach((solntile) => {
-    const id = solntile.id.replace('S', '')
-    solntile.style.background = originalGrid[id]
-  })
-}
-
+// Updates % of tiles in correct place after every swap
 function updateHintValue (correctTilesObj) {
   const progress = document.querySelector('.progress')
   const progressBar = document.querySelector('.progress-bar')
@@ -331,18 +52,26 @@ function updateHintValue (correctTilesObj) {
   let numFalse = 0
 
   arrOfWhetherTileColorIsCorrect.forEach((bool) => {
-    if (bool) {
-      numTrue++
-    } else {
-      numFalse++
-    }
+    if (bool) { numTrue++ } else { numFalse++ }
   })
 
   const percentageOfTilesCorrect = Math.ceil((numTrue / numTiles) * 100)
   progressBar.style.width = `${percentageOfTilesCorrect}%`
   progressBar.style.ariaValuenow = `${percentageOfTilesCorrect}`
-  // progressBar.textContent = `${percentageOfTilesCorrect}%`
 }
 
 // function animateTileSwap (firstTile, secondTile) {
 // }
+
+// Fisher-Yates shuffle algorithm
+function shuffle (array) {
+  var j, x, i;
+  for (i = array.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = array[i];
+      array[i] = array[j];
+      array[j] = x;
+  }
+
+  return array;
+}
--- scripts/tiles.js
+++ scripts/tiles.js
@@ -21,3 +21,124 @@ function createSolutionGrid (originalGrid) {
     acc++
   })
 }
+
+// Shuffle around tiles
+let swapColorsObj = {}
+let swappableIndexes = []
+
+function scrambleGrid () {
+  const allTiles = document.querySelectorAll('.tile')
+  const tiles = Array.from(allTiles)
+
+  fetchAndShuffleNonFixedTiles(tiles)
+  applyShuffledGridColors(tiles)
+}
+
+function fetchAndShuffleNonFixedTiles (tiles) {
+  tiles.forEach((tile) => {
+    if (tile.classList.length === 1) {
+      swapColorsObj[tile.id] = {'oldColor': tile.style.background, 'newColor': null }
+    }
+  })
+
+  for (let index in swapColorsObj) { swappableIndexes.push(index) }
+  swappableIndexes = shuffle(swappableIndexes)
+}
+
+function applyShuffledGridColors(tiles) {
+  let swappedIndex = 0
+  for (let index in swapColorsObj) {
+    const otherTileID = parseInt(swappableIndexes[swappedIndex])
+    const otherTileColor = swapColorsObj[otherTileID]['oldColor']
+    swapColorsObj[index]['newColor'] = otherTileColor
+    swappedIndex++
+  }
+
+  tiles.forEach((tile) => {
+    if (tile.classList.length === 1) {
+      tile.style.background = swapColorsObj[tile.id]['newColor']
+    }
+  })
+}
+
+// Swap colors of two tiles
+let firstColor
+let secondColor
+let correctTilesObj = {}
+
+function addClickEventsToTiles () {
+  const allTiles = document.querySelectorAll('.tile')
+  const tiles = Array.from(allTiles)
+
+  tiles.forEach((tile) => {
+    if (tile.classList.contains('fixedtile')) {
+      removeCornerTilesFromGettingClickEvents(tiles, tile)
+    }
+  })
+
+  tiles.forEach((tile) => {
+    tile.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      if (tile.classList.contains('selected')) {
+        tile.classList.remove('selected')
+      } else {
+        tile.classList.add('selected')
+      }
+
+      swapTileColorsIfMultipleTilesSelected(tile)
+    })
+  })
+}
+
+function swapTileColorsIfMultipleTilesSelected (tile) {
+  const selectedTilesInfo = document.querySelectorAll('.selected')
+  let selectedTiles = Array.from(selectedTilesInfo)
+
+  if (selectedTiles.length === 2) {
+    const firstTile = selectedTiles[0]
+    const secondTile = selectedTiles[1]
+
+    firstColor = firstTile.style.background
+    secondColor = secondTile.style.background
+    firstTile.style.background = secondColor
+    secondTile.style.background = firstColor
+
+    // animateTileSwap(firstTile, secondTile)
+
+    firstTile.classList.remove('selected')
+    secondTile.classList.remove('selected')
+
+    checkUserAnswer(correctTilesObj)
+  }
+}
+
+// Compares two tiles
+function compareTileColors (tiles, originalIndex) {
+  tiles.forEach((tile) => {
+    const originalTileColor = originalGrid[originalIndex]
+    const currentTileColor = tile.style.background
+
+    if (currentTileColor == originalTileColor) {
+      correctTilesObj[originalIndex] = true
+    } else {
+      correctTilesObj[originalIndex] = false
+    }
+
+    originalIndex++
+  })
+}
+
+function toggleFixedTileIconColor () {
+  const fixedIcons = document.querySelectorAll('.fa-circle')
+  const icons = Array.from(fixedIcons)
+
+  icons.forEach((icon) => {
+    icon.style.color = 'black'
+  })
+}
+
+function removeCornerTilesFromGettingClickEvents (array, element) {
+  const index = array.indexOf(element)
+  array.splice(index, 1)
+}
